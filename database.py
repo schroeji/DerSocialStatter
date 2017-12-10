c8b4f92998bc32a7f944ec52c06dac108bf6af50
Jean Wanka <jm.wanka@gmail.com>
2017-12-10 10:41:41 +0100

    Simple query to calculate growth of subreddits

--- database.py
+++ database.py
@@ -73,3 +73,30 @@ class DatabaseConnection(object):
         self.conn.commit()
         self.cur.close()
         self.conn.close()
+
+    def get_all_subreddits(self):
+        # TODO: method that returns only subreddits that have data in the last day
+        """
+        returns a list of all subreddits in the database
+        """
+        self.cur.execute("SELECT DISTINCT subreddit FROM data;")
+        return [x[0] for x in self.cur.fetchall()]
+
+    def get_metrics_for_subreddit(self, subreddit, timeinterval=None):
+        # TODO custom timeinterval, minimum time distance
+        """
+        gets two last values from subscribers, submissions and comment_rate
+
+        Args:
+            subreddit: string
+            timeinterval: tuple of timestamp or None, if none take last and second to last timestamp
+
+        Returns:
+            List of Tuples containing metrics from subreddit, time is decreasing
+        """
+        if timeinterval is None:
+            querystr = "SELECT subscribers, submissions, comment_rate FROM data WHERE subreddit=\'{}\' \
+                    AND end_time in (SELECT DISTINCT end_time FROM data ORDER BY end_time DESC LIMIT 2) \
+                    ORDER BY end_time DESC LIMIT 2".format(subreddit)
+        self.cur.execute(querystr)
+        return self.cur.fetchall()
