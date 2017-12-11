5a2ec3f6bc4f2082b98c9bf104166f9c59b4e06e
schroeji <schroeji@studi.informatik.uni-stuttgart.de>
2017-12-11 18:38:54 +0100

    Add: database password; argument parsing; comment rate start time

--- database.py
+++ database.py
@@ -6,9 +6,9 @@ class DatabaseConnection(object):
     http://initd.org/psycopg/docs/usage.html
     """
 
-    def __init__(self, dbname, user):
+    def __init__(self, dbname, user, password):
         try:
-            self.conn = psycopg2.connect("dbname={} user={}".format(dbname, user))
+            self.conn = psycopg2.connect(dbname=dbname, user=user, password=password, host="localhost")
             self.cur = self.conn.cursor()
             if (not self.table_exists()):
                 self.create_table()
--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 import os.path
+import argparse
 from database import DatabaseConnection
 from reddit import RedditStats
 from coinmarketcap import CoinCap
 
 def collect(subreddit_list):
     stat = RedditStats()
-    db = DatabaseConnection("postgres", "postgres")
+    db = DatabaseConnection("postgres", "postgres", "asdfgh")
     for subreddit in subreddit_list:
         db.insert(stat.compile_dict(subreddit))
         print("Got stats for:", subreddit)
     db.close()
 
-def create_subreddit_list():
+def create_subreddit_list(num):
     """
     create a list of crypto coin subreddits
     """
     stat = RedditStats()
     coincap = CoinCap()
-    names = coincap.get_coin_names(200)
+    names = coincap.get_coin_names(num)
     # remove special characters
     for i, name in enumerate(names):
         names[i] = "".join(x for x in name if x.isalnum())
@@ -26,7 +27,7 @@ def create_subreddit_list():
 
 def write_subs_to_file(path, subreddit_list):
     string = "\n".join(subreddit_list)
-    f = open("subreddits.txt", "w")
+    f = open(path, "w")
     f.write(string)
     f.close()
 
@@ -37,11 +38,32 @@ def read_subs_from_file(path):
     return subs[:-1]
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--find_subs", default=0, type=int, action='store',
+                        help="Find crypto coin subreddits (overwrites subreddits.txt).")
+    parser.add_argument("--recreate_table", default=False, action='store_true',
+                        help="Delete and recreate the data table.")
+    parser.add_argument("--collect", default=False, action='store_true',
+                        help="Collect subreddit information into the database.")
+    args = parser.parse_args()
+    # -----------------------------------
+
+    print(args)
     file_name = "subreddits.txt"
-    if os.path.exists(file_name):
-        subs = read_subs_from_file(file_name)
-    else :
-        subs = create_subreddit_list()
+    if args.find_subs > 0:
+        subs = create_subreddit_list(args.find_subs)
         write_subs_to_file(file_name, subs)
 
-    collect(subs)
+    if args.recreate_table:
+        db = DatabaseConnection("postgres", "postgres", "asdfgh")
+        db.delete_table()
+        db.create_table()
+        db.close()
+
+    if args.collect:
+        if os.path.exists(file_name):
+            subs = read_subs_from_file(file_name)
+            collect(subs)
+        else :
+            print("Collect called but 'subreddits.txt' does not exist.")
+            print("Run --find_subs first.")
