ddae1ea9261e67c9d9e35668ed8b7ec37735a40d
schroeji <schroeji@studi.informatik.uni-stuttgart.de>
2017-12-13 00:17:57 +0100

    Add: Plotting function

--- query.py
+++ query.py
@@ -35,8 +35,7 @@ def recent_growth(db, subreddits):
             growth = calc_mean_growth(metrics)
             mean_growths.append((subr, growth))
     sorted_growths = sorted(mean_growths, key=lambda subr: subr[1])
-    for s in sorted_growths:
-        print(s)
+    return sorted_growths
 
 def growth_in_interval(db, subreddits, start, end):
     mean_growths = []
@@ -47,8 +46,7 @@ def growth_in_interval(db, subreddits, start, end):
             growth = calc_mean_growth(metrics)
             mean_growths.append((subr, growth))
     sorted_growths = sorted(mean_growths, key=lambda subr: subr[1])
-    for s in sorted_growths:
-        print(s)
+    return sorted_growths
 
 def plot_growth(db, subreddit_list, start=None, end=None):
     if start is None:
@@ -56,33 +54,34 @@ def plot_growth(db, subreddit_list, start=None, end=None):
     if end is None:
         end = datetime.datetime.now()
     assert start < end
-
-    list_of_dates = []
-    growths = []
+    print(subreddit_list)
     for subr in subreddit_list:
+        list_of_dates = []
+        growths = []
         m = db.get_rows_for_subreddit(subr, start=start, end=end)
         m.reverse()
         if (len(m) <= 2):
-            print("Not enough data points in interval.")
-            return
+            print("Not enough data points in interval for {}.".format(subr))
+            continue
         list_of_dates.append(m[0][0])
         growths.append(0)
         for i in range(1, len(m)):
             list_of_dates.append(m[i][0])
-            growths.append(growths[-1] + calc_mean_growth([m[i-1][2:], m[i][2:]]))
-    dates = matplotlib.dates.date2num(list_of_dates)
-    print(growths)
-    plt.plot_date(dates, growths)
+            growths.append(growths[-1] + calc_mean_growth([m[i][2:], m[i-1][2:]]))
+        dates = matplotlib.dates.date2num(list_of_dates)
+        plt.plot_date(dates, growths, "-", label=subr)
+    plt.legend()
     plt.show()
 
 def main():
     db = DatabaseConnection("postgres", "postgres", "mongojean", "chumbala.duckdns.org")
     all_subreddits = db.get_all_subreddits()
-    # recent_growth(db, all_subreddits)
     start =  datetime.datetime.now() - datetime.timedelta(hours=12)
     end =  datetime.datetime.now()- datetime.timedelta(hours=0)
-    growth_in_interval(db, all_subreddits, start, end)
-    plot_growth(db, ["ripple"], start, end)
+    sorted_subs = growth_in_interval(db, all_subreddits, start, end)
+    # print(sorted_subs)
+    plot_growth(db, [s[0] for s in sorted_subs[-10:]], start, end)
+    # recent_growth(db, ["ripple"])
     db.close()
 
 if __name__ == "__main__":
