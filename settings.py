0926a1410f2a59af7fcc1c7d81e5280df87ebaf7
schroeji <schroeji@studi.informatik.uni-stuttgart.de>
2018-01-02 07:46:31 +0100

    Add: csv folder

--- settings.py
+++ settings.py
@@ -1,12 +1,13 @@
 import os
 
 filedir = os.path.dirname(os.path.realpath(__file__))
+csvdir = os.path.join(filedir, "csv")
 
 #general settings
 general = dict(
-    subreddit_file = os.path.join(filedir, "subreddits.csv"),
-    binance_file = os.path.join(filedir, "binance.csv"),
-    poloniex_file = os.path.join(filedir, "poloniex.csv"),
+    subreddit_file = os.path.join(csvdir, "subreddits.csv"),
+    binance_file = os.path.join(csvdir, "binance.csv"),
+    poloniex_file = os.path.join(csvdir, "poloniex.csv"),
     log_file = os.path.join(filedir, "log.log"),
     auth_file = os.path.join(filedir, "auth.json")
 )
