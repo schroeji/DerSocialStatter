c3b6d63e4d60f317bb760c5d22d5322e88a7d85f
schroeji <schroeji@studi.informatik.uni-stuttgart.de>
2018-02-08 11:40:34 +0000

    Clean: style

--- settings.py
+++ settings.py
@@ -4,7 +4,8 @@ filedir = os.path.dirname(os.path.realpath(__file__))
 csvdir = os.path.join(filedir, "csv")
 
 #general settings
-general = dict( subreddit_file=os.path.join(csvdir, "subreddits.csv"),
+general = dict(
+    subreddit_file=os.path.join(csvdir, "subreddits.csv"),
     binance_file=os.path.join(csvdir, "binance.csv"),
     poloniex_file=os.path.join(csvdir, "poloniex.csv"),
     bittrex_file=os.path.join(csvdir, "bittrex.csv"),
